77d73e79cac3b5f234702c55b31f135788f51ad4
Daniel Cefram Ramirez <dcefram@outlook.com>
2019-11-28 16:49:55 +0800

    feat: add basic inputrange

--- src/components/InputRange/InputRange.tsx
+++ src/components/InputRange/InputRange.tsx
@@ -1,14 +1,52 @@
-import React, { FunctionComponent, ReactElement } from 'react';
+import React, {
+  FunctionComponent,
+  ReactElement,
+  useState,
+  useEffect,
+  useCallback,
+  ChangeEvent
+} from 'react';
 import styled from '@emotion/styled';
 
 export interface InputRangeProps {
   className: string;
   children: ReactElement;
+  step?: number;
+  min?: number;
+  max?: number;
+  value?: number;
 }
+
+interface RangeButtonProps {
+  children: string;
+  onClick: any;
+}
+
+const StyledRangeContainer = styled('div')`
+  background: #111;
+  border: 1px solid #000;
+  display: flex;
+`;
+
+const StyledRangeButton = styled('button')<RangeButtonProps>`
+  background: transparent;
+  border: 0;
+  color: #666666;
+  cursor: pointer;
+  height: 14px;
+  outline: none;
+  padding: 0;
+  width: 14px;
+
+  &:hover {
+    color: #fff;
+  }
+`;
 
 const StyledInputRange = styled('input')<InputRangeProps | { type: string }>`
   -webkit-appearance: none;
   background: transparent;
+  margin: 0;
   width: 100%;
 
   &:focus {
@@ -17,29 +55,64 @@ const StyledInputRange = styled('input')<InputRangeProps | { type: string }>`
 
   &::-webkit-slider-thumb {
     -webkit-appearance: none;
-    background: #ffffff;
+    background: #888888;
     border-radius: 0;
-    border: 1px solid #000000;
-    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
     cursor: pointer;
-    height: 36px;
-    margin-top: -14px;
-    width: 16px;
+    height: 12px;
+    margin-top: 1px;
+    width: 12px;
+
+    &:hover {
+      background: #fff;
+    }
   }
 
   &::-webkit-slider-runnable-track {
-    width: 100%;
-    height: 8.4px;
+    background: transparent;
+    border: 0;
     cursor: pointer;
-    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-    background: #3071a9;
-    border-radius: 1.3px;
-    border: 0.2px solid #010101;
+    height: 14px;
+    width: 100%;
   }
 `;
 
-const InputRange: FunctionComponent<InputRangeProps> = ({ children, ...rest }) => (
-  <StyledInputRange type="range" {...rest} />
-);
+const InputRange: FunctionComponent<InputRangeProps> = ({
+  children,
+  value: propValue = 0,
+  step = 1,
+  min = 0,
+  max = 100,
+  ...rest
+}) => {
+  const [value, setValue] = useState(propValue);
+
+  useEffect(() => {
+    setValue(Number(propValue) || 0);
+  }, [propValue]);
+
+  const handleButtonClick = (offset: number) => () => {
+    const newValue = value + offset;
+    setValue(Math.min(max, Math.max(min, newValue)));
+  };
+
+  const handleRangeChange = (event: ChangeEvent<HTMLInputElement>) =>
+    setValue(Number(event.target.value));
+
+  return (
+    <StyledRangeContainer>
+      <StyledRangeButton onClick={handleButtonClick(-step)}>-</StyledRangeButton>
+      <StyledInputRange
+        type="range"
+        value={value}
+        min={min}
+        max={max}
+        step={step}
+        onChange={handleRangeChange}
+        {...rest}
+      />
+      <StyledRangeButton onClick={handleButtonClick(step)}>+</StyledRangeButton>
+    </StyledRangeContainer>
+  );
+};
 
 export default InputRange;
--- src/components/InputRange/index.ts
+++ src/components/InputRange/index.ts
@@ -1,1 +1,1 @@
-export { default } from './InputRange';
+export { default, InputRangeProps } from './InputRange';
