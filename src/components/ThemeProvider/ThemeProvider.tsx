12c013a9bbe8f31a63640f63e325ebdf2a8a54f7
Daniel Cefram Ramirez <dcefram@outlook.com>
2020-02-27 22:58:26 +0800

    chore: update docs

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import { Global, css } from '@emotion/core';
 import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
 
