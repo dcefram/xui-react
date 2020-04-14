24cf68309c042af1735a12659f6498741334e280
Daniel Cefram Ramirez <dcefram@outlook.com>
2020-04-14 22:37:34 +0800

    fix: add styling for scrollbars

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -15,6 +15,31 @@ function ThemeProvider<Theme>({ theme, children }: ThemeProviderProps<Theme>): R
           body {
             font-family: roboto, 'Malgun Gothic', sans-serif;
           }
+          
+          ::-webkit-scrollbar {
+            width: 12px;
+            cursor: pointer;
+          }
+          
+          ::-webkit-scrollbar-button {
+            background-color: #000;
+            background-repeat: no-repeat;
+            background-position: center;
+            height: 10px;
+            width: 10px;
+          }
+          
+          ::-webkit-scrollbar-thumb {
+            background-color: #393939;
+            border: 1px solid #000;
+            cursor: pointer;
+            width: 10px;
+          }
+          
+          ::-webkit-scrollbar-track {
+            background-color: #000;
+            width: 12px;
+          }
         `}
       />
       <EmotionThemeProvider theme={theme || {}}>{children}</EmotionThemeProvider>
